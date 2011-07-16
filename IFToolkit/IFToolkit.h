a4a39d94379c77719be3299c1faa6a8917e47b51
Itai Ferber <itaiferber@gmail.com>
2011-07-15 23:29:45 -0400

    Initial Commit

---
+++ IFToolkit/IFToolkit.h
@@ -0,0 +1,13 @@
+//
+//  IFToolkit.h
+//  IFToolkit
+//
+//  Created by Itai Ferber on 7/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IFToolkit : NSObject
+
+@end
---
+++ IFToolkit/IFToolkit.m
@@ -0,0 +1,23 @@
+//
+//  IFToolkit.m
+//  IFToolkit
+//
+//  Created by Itai Ferber on 7/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "IFToolkit.h"
+
+@implementation IFToolkit
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
