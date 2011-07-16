f4a84791e8c150b2928884c42d89abdd062502ec
Itai Ferber <itaiferber@gmail.com>
2011-07-16 13:19:16 -0400

    Added imports in IFToolkitFramework.h and made headers public.
    -- Added all necessary import statements in IFToolkitFramework.h.
    -- Set the level of all headers to public.

--- IFToolkit/IFToolkitFramework.h
+++ IFToolkit/IFToolkitFramework.h
@@ -9,6 +9,18 @@
 #ifndef IFToolkit_IFToolkitFramework_h
 #define IFToolkit_IFToolkitFramework_h
 
+#import "IFPrint.h"
+
+#import "IFSheetController.h"
+#import "IFSheetControllerDelegate.h"
+
 #import "IFToolbarManager.h"
+#import "IFToolbarManagerDelegate.h"
+#import "IFToolbarPane.h"
+#import "NSWindow+Resizing.h"
+
+#if !__has_feature(objc_arc)
+#import "MAZeroingWeakRef.h"
+#endif
 
 #endif
